d7cda493d81cbbfe6cdb6b4626939e390fd130b3
Phuong <phuongtranpt0606@gmail.com>
2021-03-02 21:56:58 +0700

    Bai 1,2,3,4 : Khai pha luat ket hop

---
+++ src/wekapro/MyAprioriModel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wekapro;
+
+import weka.associations.Apriori;
+import weka.core.Instances;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MyAprioriModel extends MyKnowledgeModel{
+    Apriori apriori;
+    Instances newData;
+
+    public MyAprioriModel() {
+    }
+
+    public MyAprioriModel(String filename, String m_opts, String d_opts) throws Exception {
+        super(filename, m_opts, d_opts);
+        this.apriori = new Apriori();
+    }
+    
+    public void mineAssociationRules() throws Exception{
+        //Loc du lieu
+//        this.newData = removeData(this.dataset);
+        this.newData = convertData(this.dataset);
+        //Thiet lap thong so cho model Apriori
+        apriori.setOptions(this.model_options);
+        //Khai pha luat ket hop bang thuat toan Apriori
+        apriori.buildAssociations(this.newData);
+    }
+
+    @Override
+    public String toString() {
+        return apriori.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
---
+++ src/wekapro/WekaPro.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wekapro;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class WekaPro {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+//        MyKnowledgeModel model = new MyKnowledgeModel (
+//                "C:\\Program Files\\Weka-3-8-5\\data\\iris.arff");
+//        System.out.println(model);
+//        model.saveData("D:\\Test\\iris.arff");
+//        model.saveData2CSV("D:\\Test\\iris_CSV.csv");
+//        MyAprioriModel model = new MyAprioriModel(
+//                            "C:\\Program Files\\Weka-3-8-5\\data\\weather.numeric.arff",
+//                            "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
+//                            "-R 2-3");
+//        model.mineAssociationRules();
+
+        MyFPGrowthModel model = new MyFPGrowthModel(
+                "C:\\Program Files\\Weka-3-8-5\\data\\weather.nominal.arff",
+                "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
+                "-N -R first-last");
+        model.mineAssociationRules();
+        System.out.println(model);
+    }
+    
+}
